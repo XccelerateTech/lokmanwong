401e976c4c9425366eed46ffc63423f822d1c3ac
lmwong9 <43207626+lmwong9@users.noreply.github.com>
2018-09-30 21:18:04 +0800

    upload exH.js

---
+++ exh.js
@@ -0,0 +1,23 @@
+function roadsign(operator,x,y) {
+	switch (operator) {
+		case "+":
+		return x + y;
+		break;
+
+		case "-":
+		return x - y;
+		break;
+
+		case "*":
+		return x * y;
+		break;
+
+		case "/":
+		return x / y;
+		break;
+
+		default: console.log('please enter valid variables');
+
+	}
+}
+
