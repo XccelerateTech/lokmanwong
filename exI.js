55197310c73ba50b4aac1a6479825c312674a93e
lmwong9 <43207626+lmwong9@users.noreply.github.com>
2018-09-30 22:58:24 +0800

    uploading exercise i

---
+++ exI.js
@@ -0,0 +1,10 @@
+for (var i = 1; i < 31; i++) {
+   if (i % 3 === 0 && i % 5 === 0) {
+    console.log("hong Kong");
+} else if (i % 5 === 0) {
+    console.log("kong"); 
+} else if (i % 3 === 0) {
+	console.log ("Hong"); 
+} else {console.log(i);}
+	}
+
